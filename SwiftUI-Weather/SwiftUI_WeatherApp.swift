7458d3ab070157d63ce004b5b2d42e12c6ab7d0b
Aleksandar Mijadzevic <aleksandar.mijadzevic@pearshadow.com>
2022-01-04 19:35:10 +0100

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Aleksandar Mijadzevic on 4. 1. 2022..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
